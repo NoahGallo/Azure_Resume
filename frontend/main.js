5a9665ece594cdfbd6699706995464821f9c5a83
NoahGallo <gallo.noah@gmail.com>
2024-04-07 14:15:44 +0200

    Created Azure Function App, website is now hosted on a static website on Azure Blob Storage

--- frontend/main.js
+++ frontend/main.js
@@ -2,11 +2,12 @@ window.addEventListener('DOMContentLoaded', (event) =>{
     getVisitCount();
 })
 
-const functionApi = 'http://localhost:7071/api/GetResumeCounter';
+const functionApiUrl = 'https://noahgetresumecounter.azurewebsites.net/api/GetResumeCounter?code=gXH1LF3jhzaNzoiM5muyJXO2XAvZDwcRtwVy102JYyGsAzFuU9e_cw=='
+const localFunctionApi = 'http://localhost:7071/api/GetResumeCounter';
 
 const getVisitCount = () => {
     let count = 30;
-    fetch(functionApi).then(response => {
+    fetch(functionApiUrl).then(response => {
         return response.json()
     }).then(response =>{
         console.log("Website called function API.");
